16f1c190aab6da31a8b9d0b8a8b8e025b35552dd
YusupovAr <artur.yusupov.97@list.ru>
2023-02-06 21:35:39 +0300

    enter

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,9 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 function mySwiper() {
     if (window.matchMedia('(width < 768px)').matches) {
         new Swiper('.swiper', {
+            modules: [Navigation, Pagination],
             slidesPerView: 'auto',
             pagination: {
                 el: '.swiper-pagination',
